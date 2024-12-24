0650ebdae8f5b15a1406366ba0032d30996998d2
Haseeb Ur Rehman <haseebanis12345@gmail.com>
2024-12-24 11:08:14 +0500

    login/signup form

---
+++ Login-SignUp-Form/Login-SignUp-Form/Models/LoginSignupContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login_SignUp_Form.Models;
+
+public partial class LoginSignupContext : DbContext
+{
+    public LoginSignupContext()
+    {
+    }
+
+    public LoginSignupContext(DbContextOptions<LoginSignupContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3213E83F54F02839");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__AB6E61642C485E2E").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .HasColumnName("firstName");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(10)
+                .HasColumnName("gender");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .HasColumnName("lastName");
+            entity.Property(e => e.Password)
+                .HasMaxLength(100)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Login-SignUp-Form/Login-SignUp-Form/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Login_SignUp_Form.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    public string LastName { get; set; } = null!;
+
+    [Required]
+    public string Gender { get; set; } = null!;
+
+    [Required]
+    public int? Age { get; set; }
+
+    [Required]
+    public string Email { get; set; } = null!;
+
+    [DataType(DataType.Password)]
+    [Required]
+    public string Password { get; set; } = null!;
+}
